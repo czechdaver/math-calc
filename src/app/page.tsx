5982353f9414b2187f84cc7d47b4027b1a7bd82d
czechdaver <czechdaver@gmail.com>
2025-07-20 18:15:15 +0000

    Changes before Firebase Studio auto-run

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+
+const SUPPORTED_LOCALES = ['cs', 'hu', 'pl', 'sk'];
+const DEFAULT_LOCALE = 'cs';
+
+export default function Page() {
+  const headersList = headers();
+  const acceptLanguage = headersList.get('accept-language');
+
+  let locale = DEFAULT_LOCALE;
+  if (acceptLanguage) {
+    const languages = acceptLanguage.split(',').map(lang => lang.split(';')[0]);
+    for (const lang of languages) {
+      if (SUPPORTED_LOCALES.includes(lang)) {
+        locale = lang;
+        break;
+      }
+    }
+  }
+
+  redirect(`/${locale}`);
+}

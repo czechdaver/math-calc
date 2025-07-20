1eab20f2d8ce52ef4ddd23048ecfab58f2f1ab47
czechdaver <czechdaver@gmail.com>
2025-07-20 17:44:01 +0000

    Changes before Firebase Studio auto-run

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['cs', 'sk', 'pl', 'hu'],
-    defaultLocale: 'cs',
-    localeDetection: true,
-  },
 };
 
 module.exports = nextConfig;
